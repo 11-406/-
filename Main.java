16d9c813c7642690796e2a34eec09dc74312da12
P1lox <marselpilox17@rambler.ru>
2025-04-27 15:11:48 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        start();
+    }
+
+    public static void start(){
+        CreateFile createFile = new CreateFile();
+        createFile.fillFile();
+        try(Scanner scanner = new Scanner(new File("fileNumber.txt"))){
+            List<Integer> allNumbers = new ArrayList<>();
+            while (scanner.hasNextInt()) {
+                allNumbers.add(scanner.nextInt());
+            }
+            int currentIndex = 0;
+            for(int i = 0; i < 60; i++){
+                int size = createFile.getList()[i];
+                int[] numbers = new int[size];
+                for (int j = 0; j < size && currentIndex < allNumbers.size(); j++) {
+                    numbers[j] = allNumbers.get(currentIndex);
+                    currentIndex++;
+                }
+                long time = System.nanoTime();
+                MergeSort.mergeSort(numbers);
+                long time2 = System.nanoTime();
+
+                System.out.println("Количество элементов " + size + " время: " + (time2 - time) + " количество итераций: " + MergeSort.getMergeIterations());
+
+
+            }
+
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Файл не найден: " + e.getMessage());
+        }
+    }
+
+}
